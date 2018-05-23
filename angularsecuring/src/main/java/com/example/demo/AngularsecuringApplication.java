16e7204ce442da7a5bf162f4de5461bd65ca9391
sissa <sissa@LT-SOD00004200.sodifrance.fr>
2018-05-23 10:50:50 +0200

    First commit

---
+++ angularsecuring/src/main/java/com/example/demo/AngularsecuringApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AngularsecuringApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularsecuringApplication.class, args);
+	}
+	
+	
+}
